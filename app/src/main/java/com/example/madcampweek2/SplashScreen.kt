b2223609c1d893979ae48126bc690c73a17e7141
kadiace <rlacjfghks95@gmail.com>
2021-01-08 02:44:11 +0900

    connection success!!!!!

--- app/src/main/java/com/example/madcampweek2/SplashScreen.kt
+++ app/src/main/java/com/example/madcampweek2/SplashScreen.kt
@@ -1,10 +1,19 @@
 package com.example.madcampweek2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.madcampweek2.Fragment3.GameActivity2
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_splash_screen.*
+import org.json.JSONException
+import org.json.JSONObject
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +21,20 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         button.setOnClickListener {
-            if (true) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+//            if (true) {
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+//                finish()
+//            }
+            VolleyService.testVolley(this) {testSuccess ->
+                if (testSuccess) {
+                    Toast.makeText(this, "통신 성공!", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "통신 실패!", Toast.LENGTH_LONG).show()
+                }
             }
         }
     }
 }
+
+
---
+++ app/src/main/java/com/example/madcampweek2/VolleyService.kt
@@ -0,0 +1,32 @@
+package com.example.madcampweek2
+
+import android.content.Context
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+
+object VolleyService {
+
+    val testUrl = "http://192.249.18.240:3000/"
+
+    fun testVolley(context: Context, success: (Boolean) -> Unit) {
+        val myJson = JSONObject()
+        val requestBody = myJson.toString()
+
+        val testRequest = object : StringRequest(Method.GET, testUrl, Response.Listener {response ->
+            success(true)
+        }, Response.ErrorListener { error ->
+            success(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+        }
+        Volley.newRequestQueue(context).add(testRequest)
+    }
+}
