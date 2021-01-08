5b9bea4916b437615b0b743f858d4b53c7b51200
kadiace <rlacjfghks95@gmail.com>
2021-01-08 15:34:39 +0900

    add dispatch event at splach screen

--- app/src/main/java/com/example/madcampweek2/SplashScreen.kt
+++ app/src/main/java/com/example/madcampweek2/SplashScreen.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -11,6 +12,9 @@ import android.provider.Settings
 import android.telephony.PhoneNumberUtils
 import android.telephony.TelephonyManager
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -50,6 +54,23 @@ class SplashScreen : AppCompatActivity() {
             val password : String = password.text.toString()
             loginVolley(this, url, id, password)
         }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val focusView: View? = currentFocus
+        if (focusView != null) {
+            val rect = Rect()
+            focusView.getGlobalVisibleRect(rect)
+            val x = ev.x.toInt()
+            val y = ev.y.toInt()
+            if (!rect.contains(x, y)) {
+                val imm: InputMethodManager =
+                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(focusView.getWindowToken(), 0)
+                focusView.clearFocus()
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 
     private fun loginVolley(context: Context, url: String, userid: String, password: String) {
